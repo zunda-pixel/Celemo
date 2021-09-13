3140fbd6f64ddb6cea5b7cd5eb0ec7c2778bbcbe
zunda-pixel <47569369+zunda-pixel@users.noreply.github.com>
2021-09-13 21:26:44 +0900

    Initial Commit

---
+++ sRemo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  sRemo
+//
+//  Created by zunda on 2021/09/13.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ sRemo/sRemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  sRemoApp.swift
+//  sRemo
+//
+//  Created by zunda on 2021/09/13.
+//
+
+import SwiftUI
+
+@main
+struct sRemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
